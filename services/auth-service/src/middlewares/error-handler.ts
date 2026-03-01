8404a85bce1af01e998a5d1dcd124b255a81473f
Shinjon Chakraborty <toshinjonc2006@gmail.com>
2026-03-01 17:50:33 +0530

    feat(common): setup error handler & request validator

---
+++ services/auth-service/src/middlewares/error-handler.ts
@@ -0,0 +1,23 @@
+import { HttpError } from "@ecommerce-platform/common";
+
+import type { ErrorRequestHandler } from "express";
+
+import logger from "@/utils/logger";
+
+export const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
+  logger.error({ err }, "Unhandled error occurred");
+
+  const error = err instanceof HttpError ? err : undefined;
+  const statusCode = error?.statusCode || 500;
+  const message =
+    statusCode >= 500
+      ? "Internal Server Error"
+      : (error?.message ?? "Unknown Error");
+  const payload = error?.details
+    ? { message, details: error.details }
+    : { message };
+
+  res.status(statusCode).json(payload);
+
+  void _next();
+};
