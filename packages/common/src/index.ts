b7142457f2969f43ec011a9949dd9f41345de105
Shinjon Chakraborty <toshinjonc2006@gmail.com>
2026-03-01 17:50:55 +0530

    feat(auth-service): setup auth db

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -2,3 +2,4 @@ export * from "./logger";
 export * from "./env";
 export type { Logger } from "pino";
 export { z } from "zod";
+export * from "./errors/http-error";
--- services/auth-service/src/app.ts
+++ services/auth-service/src/app.ts
@@ -1,7 +1,23 @@
-import express, { type Application } from "express";
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import { errorHandler } from "@/middlewares/error-handler";
 
-export const createApp = (): Application => {
+export const createApp = (): express.Application => {
   const app = express();
+  app.use(
+    cors({
+      origin: "*",
+      credentials: true,
+    }),
+  );
+  app.use(helmet());
+  app.use(express.json());
+
+  app.use((_req, res) => {
+    res.status(404).json({ message: "Not Found" });
+  });
+  app.use(errorHandler);
 
   return app;
 };
